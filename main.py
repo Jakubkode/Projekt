e4f2010772877806ad1b6aec502cc8f4e85300d4
Jakubkode <jakubbiala@gmail.com>
2025-06-24 10:55:26 +0200

    punkt turystyczny

--- main.py
+++ main.py
@@ -7,3 +7,18 @@ from bs4 import BeautifulSoup
 punkty_turystyczne = []
 przewodnicy = []
 klienci = []
+
+class PunktTurystyczny:
+    def __init__(self, nazwa, lokalizacja):
+        self.nazwa = nazwa
+        self.lokalizacja = lokalizacja
+        self.wspolrzedne = self.pobierz_wspolrzedne()
+        self.marker = map_widget.set_marker(self.wspolrzedne[0], self.wspolrzedne[1], text=f"Punkt: {self.nazwa}")
+
+    def pobierz_wspolrzedne(self):
+        url = f"https://pl.wikipedia.org/wiki/{self.lokalizacja}"
+        response = requests.get(url).text
+        soup = BeautifulSoup(response, "html.parser")
+        latitude = float(soup.select(".latitude")[1].text.replace(",", "."))
+        longitude = float(soup.select(".longitude")[1].text.replace(",", "."))
+        return [latitude, longitude]
